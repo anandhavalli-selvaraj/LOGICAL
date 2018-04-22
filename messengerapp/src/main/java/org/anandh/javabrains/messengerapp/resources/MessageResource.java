ae2b1b21a019d625cb4596eba07360bddd48622f
Anandhavalli <anandhavallijl@gmail.com>
2018-04-22 17:27:34 +0530

    redmine #4 content negotiation in MessageResouce class

--- messengerapp/src/main/java/org/anandh/javabrains/messengerapp/resources/MessageResource.java
+++ messengerapp/src/main/java/org/anandh/javabrains/messengerapp/resources/MessageResource.java
@@ -1,7 +1,5 @@
 package org.anandh.javabrains.messengerapp.resources;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
 
 import javax.ws.rs.BeanParam;
@@ -13,12 +11,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.core.Response.Status;
 
 import org.anandh.javabrains.messengerapp.model.Message;
 import org.anandh.javabrains.messengerapp.resources.beans.MessageFilterBean;
@@ -31,8 +27,10 @@ public class MessageResource {
    private MessageService messageService=new MessageService();
 	
 	@GET
-    public List<Message> getMessages(@BeanParam MessageFilterBean messageFilterBean)
+	@Produces(MediaType.APPLICATION_JSON)
+    public List<Message> getJSONMessages(@BeanParam MessageFilterBean messageFilterBean)
 	{
+		System.out.println("JSON Method called");
 		if(messageFilterBean.getYear()>0)
 		{
 			return messageService.getAllMessagesForYear(messageFilterBean.getYear());
@@ -43,17 +41,51 @@ public class MessageResource {
 		}
 		List<Message>messageList=messageService.getAllMessages();
 		System.out.println(messageList);
+		System.out.println("JSON Method called");
+
 		return messageList;
 	}
+	@GET
+	@Produces(MediaType.TEXT_XML)
+    public List<Message> getXMLMessages(@BeanParam MessageFilterBean messageFilterBean)
+	{
+		System.out.println("XML Method called");
+		if(messageFilterBean.getYear()>0)
+		{
+			return messageService.getAllMessagesForYear(messageFilterBean.getYear());
+		}
+		if(messageFilterBean.getIndex()>=0&messageFilterBean.getLimit()>0)
+		{
+			return messageService.getAllMessagesPagination(messageFilterBean.getIndex(), messageFilterBean.getLimit());
+		}
+		List<Message>messageList=messageService.getAllMessages();
+		System.out.println(messageList);
+		System.out.println("JSON Method called");
+		return messageList;
+	}
+	
 	
 	@GET
 	@Path("/{messageId}")
 	public Message getMessage (@PathParam("messageId") Long messageId,@Context UriInfo uriInfo)
 	{
 		Message message=messageService.getMessage(messageId) ;
-		String uri = getURISelf(messageId, uriInfo);
-		message.addLink(uri, "self");
+		message.addLink(getURISelf(messageId, uriInfo), "self");
+		String profileURI=getURIProfile(message,uriInfo);
+		message.addLink(profileURI, "profile");
+		String commentURI=getCommentURI(message,uriInfo);
+		message.addLink(commentURI, "comment");
 		return message;
+	}
+
+	private String getCommentURI(Message message, UriInfo uriInfo) {
+		String commentUri=uriInfo.getBaseUriBuilder().path(MessageResource.class).path(MessageResource.class,"getCommentResource").resolveTemplate("messageId", message.getId()).build().toString();
+		return commentUri;
+	}
+
+	private String getURIProfile(Message message, UriInfo uriInfo) {
+		String profileURL=uriInfo.getBaseUriBuilder().path(ProfileResource.class).path(message.getAuthor()).toString();
+		return profileURL;
 	}
 
 	private String getURISelf(Long messageId, UriInfo uriInfo) {
@@ -63,6 +95,8 @@ public class MessageResource {
 	
 	
 	@POST
+	@Consumes(MediaType.TEXT_XML)
+	@Produces(MediaType.TEXT_XML)
 	public Response addMessage(Message message,@Context UriInfo uriInfo) 
 	{
 		 System.out.println(uriInfo.getAbsolutePath());
